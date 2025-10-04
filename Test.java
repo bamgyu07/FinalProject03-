e6eec7318eb0025b0441dc445ecdb7913d298387
bamgyu07 <alyamafaza7@gmail.com>
2025-10-04 13:07:51 +0700

    Ini baris kode kedua

---
+++ Test.java
@@ -0,0 +1,6 @@
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hi, ini baris kode pertama");
+        System.out.println("Ini baris kode kedua");
+    }
+}
