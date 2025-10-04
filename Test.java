a14995741da1bffdca303945bf106f33fe0143f8
bamgyu07 <alyamafaza7@gmail.com>
2025-10-04 13:08:16 +0700

    ini baris ke tiga

--- Test.java
+++ Test.java
@@ -2,5 +2,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Hi, ini baris kode pertama");
         System.out.println("Ini baris kode kedua");
+        System.out.println("Ini baris kode ketiga");
     }
 }
